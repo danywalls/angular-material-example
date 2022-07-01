513816ba9ddeca11e96fe35fd62223e7fb37bf45
Dany Sebastián Paredes De Los Santos <dany.paredes@external.wolterskluwer.com>
2022-07-01 16:44:43 +0200

    create table and navigation

---
+++ src/app/stats/stats.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { StatsDataSource, StatsItem } from './stats-datasource';
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.css']
+})
+export class StatsComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<StatsItem>;
+  dataSource: StatsDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new StatsDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
